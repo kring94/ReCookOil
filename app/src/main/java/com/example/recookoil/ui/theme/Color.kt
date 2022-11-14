bf0f4f79db6311f334de0784ef99a9e274955e9b
Ronald Stivel Melo Cardenas <ronald9412@gmail.com>
2022-11-14 15:12:17 -0500

    Se ha implementado logo de la app, y layout del login sin funcionalidad

---
+++ app/src/main/java/com/example/recookoil/ui/theme/Color.kt
@@ -0,0 +1,19 @@
+package com.example.recookoil.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple200 = Color(0xFFBB86FC)
+val Purple500 = Color(0xFF6200EE)
+val Purple700 = Color(0xFF3700B3)
+val Teal200 = Color(0xFF03DAC5)
+
+val Primary = Color(0xff93c01f)
+val PrimaryDisable = Color(0xFFA9BE70)
+val PrimaryDark = Color(0xff608f00)
+val PrimaryLight = Color(0xffc7f357)
+val Secondary = Color(0xffef9d01)
+val SecondaryDark = Color(0xffb76f00)
+val SecondaryLight = Color(0xffffce48)
+
+val Gray = Color(0xffe0e0e0)
+val DarkGray = Color(0xffaeaeae)
